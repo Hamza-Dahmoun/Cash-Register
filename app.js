365192946a63e2867b40e90a78f21908a3bd2434
Hamza <hamza.dahmoun7@gmail.com>
2019-12-22 11:25:37 +0100

    write getEntries() to get input data

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ as the value of the change key.
 function checkCashRegister(price, cash, cid) {
     var change;
     // Here is your change, ma'am.
+    getEntries()
     return change;
   }
   
@@ -33,7 +34,16 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); 
+  //checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); 
 
-
+  function getEntries(){
+    price = parseFloat(document.getElementById("priceInput").value);
+    cash = parseFloat(document.getElementById("cashInput").value);
+    let tableInputs = document.querySelectorAll("table input");
+    for(let i=0; i<tableInputs.length; i++){
+        let currencyUnit = tableInputs[i].id.toString().toUpperCase().replace("_", " ");
+        let value = parseFloat(tableInputs[i].value);
+        cid.push([currencyUnit, value]);
+    }
+}
  
