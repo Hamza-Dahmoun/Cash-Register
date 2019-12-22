768a98ba3a76f0c0930733123a31cc4960f76b76
Hamza <hamza.dahmoun7@gmail.com>
2019-12-22 11:22:06 +0100

    change input id in table to match future cid array variable

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for th
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order,
 as the value of the change key. 
  */
+
+ var price, cash, cid=[];
 function checkCashRegister(price, cash, cid) {
     var change;
     // Here is your change, ma'am.
@@ -32,3 +34,6 @@ function checkCashRegister(price, cash, cid) {
   // ["ONE HUNDRED", 100]]
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]); 
+
+
+ 
